a07057045ef6728e57501e31b831e3bfd1a29d26
lucaslatessa <lucaslatessa@gmail.com>
2024-07-13 22:13:52 -0300

    delete back

--- BackEnd/usuarios/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Usuario
-
-@admin.register(Usuario)
-class UsuarioAdmin(admin.ModelAdmin):
-    list_display = ('user_id','dni','nombre', 'apellido', 'email', 'nickname', 'fecha_creacion')
-    search_fields = ('user_id','dni','nombre', 'apellido', 'email', 'nickname')
