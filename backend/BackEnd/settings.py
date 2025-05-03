841c4a2f79548dde30a81a8e7443ad0ec9e65d87
lucaslatessa <lucaslatessa@gmail.com>
2025-05-03 17:09:40 -0300

    vuelta a whitenoise(funcionaba)

--- backend/BackEnd/settings.py
+++ backend/BackEnd/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
 
 # Middleware de la aplicación
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
